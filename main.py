8165e3b6c9479b839d45286690bf84dbd756625a
Adonics Nikolett <nikolett990106@gmail.com>
2022-06-08 16:33:01 +0200

    proba

---
+++ main.py
@@ -0,0 +1,19 @@
+from git import Repo
+
+repo = Repo(self.rorepo.working_tree_dir)
+assert not repo.bare
+
+
+
+#full_local_path = "/"
+#username = "nixikee"
+#password = "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"
+#remote = f"https://{username}:{password}@github.com/some-account/some-repo.git"
+# f"https://github.com/nixikee/probafeladat"
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
